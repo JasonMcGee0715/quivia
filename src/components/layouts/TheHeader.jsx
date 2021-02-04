8fa58c5bc1712a3c2f35d630d6948ff1d5a61b4e
Sean Yager <contact@seanyager.com>
2021-02-03 22:17:49 -0600

    created 'TheHeader' layout component

---
+++ src/components/layouts/TheHeader.jsx
@@ -0,0 +1,8 @@
+function TheHeader() {
+    return (
+        <header>
+            <img src='../app/brainium-logo2.png' alt="" />
+            <h1>Wrinkle Your Brain</h1>
+        </header>
+    );
+  }
